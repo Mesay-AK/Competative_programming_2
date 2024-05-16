f708eb196fc599613f8e6f33dcdfd2124024201a
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-05-16 07:44:11 +0300

    Created 'D - Maximum Number of Zeroes' by A2SV Hub

---
+++ A2SV G5 - Contest #10 16-May-2024/D - Maximum Number of Zeroes 151795.py
@@ -0,0 +1,27 @@
+# Problem: D - Maximum Number of Zeroes - https://codeforces.com/gym/514644/problem/D
+
+from collections import defaultdict
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+c = defaultdict(int)
+
+def gcd(a,b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+extras = 0
+for i in range(n):
+    if a[i] != 0:
+        res = gcd(b[i],a[i])
+        t = (b[i]//res , a[i]//res)
+        c[t] += 1
+    elif a[i] == 0 and b[i] == 0:
+        extras += 1
+vals = list(c.values())
+if len(vals):
+    print(max(vals)+extras)
+else:
+    print(extras)
