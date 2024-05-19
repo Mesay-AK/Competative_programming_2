58eb1001f6a213bcd352ef0b650294fa77849bb3
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-05-19 21:24:17 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 19-May-2024/Cirno's Perfect Bitmasks Classroom 154871.py
@@ -0,0 +1,24 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+n = int(input())
+
+for i in range(n):
+    num = int(input())
+    if num == 1:
+        print(3)
+        continue
+
+    res = 0
+    num = bin(num)[2:]
+
+    for l in range(len(num)-1, -1, -1):
+        if num[l] == '1':
+            break
+        res += 1
+
+    if res == len(num)-1:
+        res = 2**res + 1
+        print(res)
+
+    else:
+        print(2**res)
