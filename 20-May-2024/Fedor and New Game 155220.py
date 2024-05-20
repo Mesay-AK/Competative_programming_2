9d1c967b06e79b787d582574acafc5f8c5f17ba4
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-05-20 15:41:47 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 20-May-2024/Fedor and New Game 155220.py
@@ -0,0 +1,18 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = map(int, input().split())
+
+armies = []
+for _ in range(m + 1):
+    army = int(input())
+    armies.append(army)
+
+farmy = armies.pop()
+
+friends = 0
+for army in armies:
+    difference = bin(farmy ^ army).count('1')
+    if difference <= k:
+        friends += 1
+
+print(friends)
