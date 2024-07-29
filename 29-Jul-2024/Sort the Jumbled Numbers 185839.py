2bac4e5c4b6c7129c4a273e4d9339f997cbb5c56
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-07-29 23:06:20 +0300

    Created 'Sort the Jumbled Numbers' by A2SV Hub

---
+++ 29-Jul-2024/Sort the Jumbled Numbers 185839.py
@@ -0,0 +1,35 @@
+# Problem: Sort the Jumbled Numbers - https://leetcode.com/problems/sort-the-jumbled-numbers/
+
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        mapped = [0] * len(nums)
+        num_map = defaultdict(list)
+        
+        for i in range(len(nums)):
+            number = nums[i]
+            idx = 1
+            mapped_number = 0
+            
+            while number > 0:
+                remainder = number % 10
+                mapped_number += mapping[remainder] * idx
+                number //= 10
+                idx *= 10
+            
+            if nums[i] == 0:
+                mapped_number = mapping[0]
+            
+            mapped[i] = mapped_number
+            num_map[mapped[i]].append(nums[i])
+        
+        sorted_mapped = sorted(mapped)
+        idx = 0
+        
+        for value in sorted_mapped:
+            if value in num_map:
+                for val in num_map[value]:
+                    nums[idx] = val
+                    idx += 1
+                del num_map[value]
+        
+        return nums
