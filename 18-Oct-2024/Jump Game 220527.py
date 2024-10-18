fcd09e1cb321dc5d66b137031f27328806504fff
Mesay-AK <131782426+Mesay-AK@users.noreply.github.com>
2024-10-18 16:39:34 +0300

    Created 'Jump Game' by A2SV Hub

---
+++ 18-Oct-2024/Jump Game 220527.py
@@ -0,0 +1,18 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reachable = 0
+
+        for i in range(len(nums)):
+            if i > reachable:
+                return False
+
+            reachable = max(reachable, i + nums[i])
+
+            if reachable > len(nums) - 1:
+                return True
+
+
+        return True
+
